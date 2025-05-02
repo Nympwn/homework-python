9b3383c88fe6b32dd717f4f2d5f5143d178b3471
Nympwn <yesenin_socio@mail.ru>
2025-05-02 21:47:00 +0500

    update DZ2.py
    Signed-off-by: Nympwn <yesenin_socio@mail.ru>

--- DZ2.py
+++ DZ2.py
@@ -1,21 +1,21 @@
-name = input('What is your name? \n')
-age = input('How old are you? \n')
-yesnot = input('You like to programming? Yes or no? \n')
+name = input('What is your name?\n')
+age = int(input('How old are you?\n'))
+is_like_programming = input('You like to programming? Yes or no?\n')
 
 user_info = {
     'What is your name?': name,
     'How old are you?': age,
-    'You like to programming?': yesnot
+    'You like to programming?': is_like_programming
 }
 
 print(user_info)
 
-print('Решите выражение: \ny < x - 2')
+print('Решите выражение:\ny < x - 2')
 x = int(
-    input('Введите число x: \n')
+    input('Введите число x:\n')
 )
 y = int(
-    input('Введите число y \n')
+    input('Введите число y\n')
 )
 
-print('Корректность вычисления:\n', y < x-2)
+print('Корректность вычисления:\n', y < x - 2)
