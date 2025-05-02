4092c8a51f111925120915799cccaae55199735c
Nympwn <yesenin_socio@mail.ru>
2025-05-02 21:34:37 +0500

    add study4.py
    Signed-off-by: Nympwn <yesenin_socio@mail.ru>

---
+++ DZ2.py
@@ -0,0 +1,18 @@
+name = input('What is your name? \n')
+age = input('How old are you? \n')
+yesnot = input('You like to programming? Yes or no? \n')
+
+user_info = {
+    'What is your name?': name,
+    'How old are you?': age,
+    'You like to programming?': yesnot
+}
+
+print(user_info)
+
+print('Решите выражение: \ny < x - 2')
+x = int(input('Введите число x: \n'))
+y = int(input('Введите число y \n'))
+
+print('Корректность вычисления:\n')
+print(y < x-2)
