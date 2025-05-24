58be43179c7cf5e7a81dc694b705b0e826d2010c
Nympwn <yesenin_socio@mail.ru>
2025-05-24 13:42:06 +0500

    update study5(module math and if-else).py add practice3.py (include homework for 27/05/2025 and 28/05/2025)
    Signed-off-by: Nympwn <yesenin_socio@mail.ru>

---
+++ practice3 (cicle).py
@@ -0,0 +1,115 @@
+# # Task 1
+#
+# for x in range(1, 51):
+#     if x % 2 == 0:
+#         print(x)
+#
+# # Task 2
+#
+# print(sum(range(1, 101)))
+#
+# # alternative
+# x = 0
+# for y in range(1, 101):
+#     x += y
+# print(x)
+
+# Task 3
+#
+# num = int(input('Введите натуральное число:\n'))
+# flag = True
+#
+# if num < 2:
+#     flag = False
+# else:
+#     for x in range(2, num):
+#         if num % x == 0:
+#             flag = False
+#             break
+#
+#
+# if flag:
+#     print('Число простое.')
+# else:
+#     print('Число не простое.')
+
+# Task 4
+
+# num = int(input('Введите натуральное число:\n'))
+# y = 1
+# for x in range(1, num + 1):
+#     y *= x
+# print(y)
+
+# Task 5
+# 0, 1, 1, 2, 3, 5, 8, 13, 21, 34
+# там же было про вывод, а не про расчет?
+
+# fibonachi = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+# for index, x in enumerate(fibonachi, 1):
+#     print(index, '–', x)
+
+# альтернатива
+# a, b = 0, 1
+# for _ in range(10):
+#     print(a)
+#     a, b = b, a + b
+
+# Task 6
+
+# for x in range(10, 0, -1):
+#     print(x)
+
+# Task 7
+
+# phrase = input('Введите фразу:\n')
+# присвоение букв к единому регистру (нижнему)
+# тогда не придется перечислять заглавные и строчные буквы
+# phrase = phrase.lower()
+# присвоение букв к единому регистру (верхнему)
+# phrase = phrase.upper()
+# count = 0
+# letter = ['а', 'я', 'и', 'у', 'е', 'э', 'о', 'ы', 'ё', 'ю',
+#           'А', 'Я', 'И', 'У', 'Е', 'Э', 'O', 'Ё', 'Ю']
+# sets = set(letter)
+# for letters in phrase:
+#     if letters in sets:
+#         count += 1
+# print(count)
+
+# Task 8
+
+# num = int(input('Введите число:\n'))
+# if 1000 > num > 100:
+#     num1 = num % 10
+#     num2 = num // 100
+#     num3 = num % 100 // 10
+#     mult = num1 + num2 + num3
+#     print(mult)
+# elif num > 10:
+#     num1 = num % 10
+#     num2 = num // 10
+#     mult = num1 + num2
+#     print(mult)
+# else:
+#     print('Число меньше десяти или больше тысячи')
+# в задаче не стоит определенное условие, но без циклов вижу так
+# иначе это будет бесконечное условие elif
+# надо как-то оформить в циклах...
+
+# циклы?
+# num = input('Введите число:\n')
+# total = 0
+# for digit in num:
+#     total += int(digit)
+# print(f'Сумма чисел: {total}')
+
+# Task 9
+
+# for x in range(1, 11):
+#     for y in range(1, 11):
+#         print(f'{x} * {y} = {x * y}')
+#     print()
+
+# Task 10
+
--- study6 (cicle).py
+++ study6 (cicle).py
@@ -8,6 +8,7 @@ while <condition>
 for <item> in <sequence>
    <action>
 '''
+from itertools import count
 
 # цикл while
 
@@ -64,3 +65,88 @@ seasons = {1: 'Winter', 2: 'Spring', 3: 'Summer', 4: 'Fall'}
 for s in seasons:
     print(s == 1)
 
+# range – применяется, чтобы выполнить действие заданное количество раз (начинает с 0)
+
+for x in range(3):
+    print(x)
+
+'''
+может использоваться для повторения фраз
+равносильно for i in 0, 1, 2, 3
+
+range(finish)
+range (start, finish)
+range (start, finish, step)
+start – первый элемент последовательности (включительно)
+finish – последний (не включительно)
+
+Можно указать значение приращения, добавив третий параметр
+range(2, 30, 3)
+'''
+
+for x in range (2, 30, 3):
+    print(x)
+
+# можно установить отрицательное число
+
+for x in range (50, 44, -1):
+    print(x)
+
+'''
+enumerate – считает, сколько цикл был запущен
+может принимать не обязательный аргумент (по умолчанию 0)
+'''
+
+mylist = ['milk', 'eggs', 'breat']
+for c, value in enumerate(mylist, 1):
+    print(c, value)
+
+for index, item in enumerate(['one', 'two', 'three']):
+    print(index, '::', item)
+
+'''
+Вложенный цикл – цикл в цикле
+Запускается при каждой итерации основного цикла 
+'''
+
+fruit = ['apple', 'pineapple', 'orange']
+adj = ['yellow', 'sour', 'sweet']
+
+for x in adj:
+    for y in fruit:
+        print(x, y)
+
+mytext = 'Посчитаем количество символов с пробелами'
+'''
+С помощью цикла for посчитаем количество в строке
+Зададим счетчик
+'''
+count = 0
+'''
+Будем посимвольно обходить текст
+'''
+
+for letter in mytext:
+    count +=1
+
+'''
+На каждой новой итерации в переменной letter будет храниться следующий символ предложения
+увеличиваем счетчик на один (строка 130)
+Выводим значение
+'''
+
+print(count)
+
+# Посчитаем количество слов в строке
+s = 'Посчитаем количество слов в строке'
+count = 0
+flag = 0
+for i in range(len(s)):
+    if s[i] != ' ' and flag == 0:
+        count += 1
+        flag = 1
+    else:
+        if s[i] == ' ':
+            flag = 0
+
+print(count)
