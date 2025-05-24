0cb34b8ebeacae4f4268b3f64e551d07e2be207d
Nympwn <yesenin_socio@mail.ru>
2025-05-24 20:52:18 +0500

    update practice3 (cicle).py (include homework for 24/05/2025)
    Signed-off-by: Nympwn <yesenin_socio@mail.ru>

--- practice3 (cicle).py
+++ practice3 (cicle).py
@@ -1,45 +1,45 @@
 # # Task 1
-#
-# for x in range(1, 51):
-#     if x % 2 == 0:
-#         print(x)
-#
+
+for x in range(1, 51):
+    if x % 2 == 0:
+        print(x)
+
 # # Task 2
-#
-# print(sum(range(1, 101)))
-#
-# # alternative
-# x = 0
-# for y in range(1, 101):
-#     x += y
-# print(x)
+
+print(sum(range(1, 101)))
+
+# alternative
+x = 0
+for y in range(1, 101):
+    x += y
+print(x)
 
 # Task 3
-#
-# num = int(input('Введите натуральное число:\n'))
-# flag = True
-#
-# if num < 2:
-#     flag = False
-# else:
-#     for x in range(2, num):
-#         if num % x == 0:
-#             flag = False
-#             break
-#
-#
-# if flag:
-#     print('Число простое.')
-# else:
-#     print('Число не простое.')
+
+num = int(input('Введите натуральное число:\n'))
+flag = True
+
+if num < 2:
+    flag = False
+else:
+    for x in range(2, num):
+        if num % x == 0:
+            flag = False
+            break
+
+
+if flag:
+    print('Число простое.')
+else:
+    print('Число не простое.')
 
 # Task 4
 
-# num = int(input('Введите натуральное число:\n'))
-# y = 1
-# for x in range(1, num + 1):
-#     y *= x
-# print(y)
+num = int(input('Введите натуральное число:\n'))
+y = 1
+for x in range(1, num + 1):
+    y *= x
+print(y)
 
 # Task 5
 # 0, 1, 1, 2, 3, 5, 8, 13, 21, 34
@@ -50,32 +50,32 @@
 #     print(index, '–', x)
 
 # альтернатива
-# a, b = 0, 1
-# for _ in range(10):
-#     print(a)
-#     a, b = b, a + b
+a, b = 0, 1
+for _ in range(10):
+    print(a)
+    a, b = b, a + b
 
 # Task 6
 
-# for x in range(10, 0, -1):
-#     print(x)
+for x in range(10, 0, -1):
+    print(x)
 
 # Task 7
 
-# phrase = input('Введите фразу:\n')
+phrase = input('Введите фразу:\n')
 # присвоение букв к единому регистру (нижнему)
 # тогда не придется перечислять заглавные и строчные буквы
 # phrase = phrase.lower()
 # присвоение букв к единому регистру (верхнему)
 # phrase = phrase.upper()
-# count = 0
-# letter = ['а', 'я', 'и', 'у', 'е', 'э', 'о', 'ы', 'ё', 'ю',
-#           'А', 'Я', 'И', 'У', 'Е', 'Э', 'O', 'Ё', 'Ю']
-# sets = set(letter)
-# for letters in phrase:
-#     if letters in sets:
-#         count += 1
-# print(count)
+count = 0
+letter = ['а', 'я', 'и', 'у', 'е', 'э', 'о', 'ы', 'ё', 'ю',
+          'А', 'Я', 'И', 'У', 'Е', 'Э', 'O', 'Ё', 'Ю']
+sets = set(letter)
+for letters in phrase:
+    if letters in sets:
+        count += 1
+print(count)
 
 # Task 8
 
@@ -98,33 +98,53 @@
 # надо как-то оформить в циклах...
 
 # циклы?
-# num = input('Введите число:\n')
-# total = 0
-# for digit in num:
-#     total += int(digit)
-# print(f'Сумма чисел: {total}')
+num = input('Введите число:\n')
+total = 0
+for digit in num:
+    total += int(digit)
+print(f'Сумма чисел: {total}')
 
 # Task 9
 
-# for x in range(1, 11):
-#     for y in range(1, 11):
-#         print(f'{x} * {y} = {x * y}')
-#     print()
+for x in range(1, 11):
+    for y in range(1, 11):
+        print(f'{x} * {y} = {x * y}')
+    print()
 
 # Task 10
 
-# num = [-14, -6, -3, -1, 0, 1, 2, 5, 8, 10]
-# result = []
-# for x in num:
-#     if abs(x) > 5:
-#         result.append(x)
-#
-# print(result)
+num = [-14, -6, -3, -1, 0, 1, 2, 5, 8, 10]
+result = []
+for x in num:
+    if abs(x) > 5:
+        result.append(x)
+
+print(result)
 
 
 # alternative
-# num = [-14, -6, -3, -1, 0, 1, 2, 5, 8, 10]
-# result = [x for x in num if abs(x) >5]
-# print(result)
+num = [-14, -6, -3, -1, 0, 1, 2, 5, 8, 10]
+result = [x for x in num if abs(x) >5]
+print(result)
 
 # Task 11
+
+letters = 'ЫгВЫоЯСремДШНККАыкЩЙФа'
+
+for letter in letters:
+    if letters.upper() == letters:
+        letters.replace(letter, '')
+
+print(letters.lower())
+
+# Task 12
+
+nickname = input('Укажи свой ник:\n')
+nickname_friends = ('Мавпродош', 'Лорнектиф', 'Древерол', 'Фиригарпиг', 'Клодобродыч')
+
+if nickname in nickname_friends:
+    print(f'Ты – свой. Приветствую, любезный {nickname}!')
+else:
+    while nickname_friends != nickname:
+        print('Тут ничего нет. Еще есть вопросы?')
+        # Бесконечный цикл, по заданию "так как им будет показываться это сообщение постоянно".
